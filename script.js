5b3c9226f31d7a687de2ee9c85c41f2bdac48bf2
Occams-Razar <km31188@students.mcpasd.k12.wi.us>
2025-05-15 18:52:15 -0500

    v5
    Added more responsive changes also the last one messed up

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-import LocomotiveScroll from "locomotive-scroll";
+// import LocomotiveScroll from "locomotive-scroll";
 
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-});
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("[data-scroll-container]"),
+//   smooth: true,
+// });
 
-let screenWidth = window.screenWidth;
-window.addEventListener("resize", () => {
-  screenWidth = window.innerWidth;
-});
-console.log(screenWidth);
+function updateNavbar() {
+  const navText = document.getElementsByClassName("navText");
+  const nav1 = document.getElementById("nav1");
+  const nav2 = document.getElementById("nav2");
+  const nav3 = document.getElementById("nav3");
+  const nav4 = document.getElementById("nav4");
+  const nav5 = document.getElementById("nav5");
+
+  if (window.innerWidth <= 768) {
+    nav1.innerHTML = "▲";
+    nav2.innerHTML = "▲";
+    nav3.innerHTML = "▲";
+    nav4.innerHTML = "▲";
+    nav5.innerHTML = "▲";
+    // navText.style.fontSize = "1 vw";
+  } else {
+    nav1.innerHTML = "home";
+    nav2.innerHTML = "about";
+    nav3.innerHTML = "films";
+    nav4.innerHTML = "store";
+    nav5.innerHTML = "contact";
+  }
+}
+window.addEventListener("resize", updateNavbar);
+window.addEventListener("load", updateNavbar);
