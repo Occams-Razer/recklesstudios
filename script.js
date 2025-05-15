f095db611a3e34079f275551188dbf688f73f954
Occams-Razar <km31188@students.mcpasd.k12.wi.us>
2025-05-15 18:23:45 -0500

    responsive additions v4

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
 });
+
+let screenWidth = window.screenWidth;
+window.addEventListener("resize", () => {
+  screenWidth = window.innerWidth;
+});
+console.log(screenWidth);
