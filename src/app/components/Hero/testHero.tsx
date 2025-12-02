dc248923461e34c947c6356330c7d9f56e038ef9
Maximilian Kirchstein <151471099+Occams-Razer@users.noreply.github.com>
2025-12-02 00:48:50 -0600

    Added new hero

--- src/app/components/Hero/testHero.tsx
+++ src/app/components/Hero/testHero.tsx
@@ -1,8 +1,20 @@
 import styles from "./testHero.module.css";
+import Image from "next/image";
 const testHero = () => {
   return (
     <div className={styles.main}>
-      <h2 className={styles.title}>ONE LAST SHOT</h2>
+      {/* <Image
+        fill={true}
+        objectFit="cover"
+        alt="Background photo"
+        src="/Daveed.JPG"
+        className={styles.background}
+      /> */}
+      <h2>A RECKLESS STUDIOS PRODUCTION.</h2>
+      <h2 className={styles.title}>ONE LAST SHOT.</h2>
+      <h2 className={styles.directors}>
+        ORIGNIAL SERIES BY RANGER HOWARD AND MAZI DOSSA.
+      </h2>
     </div>
   );
 };
