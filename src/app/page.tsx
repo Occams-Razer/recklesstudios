4687e85769bc1be884d0ea45c0e960a5449f4fbf
Maximilian Kirchstein <151471099+Occams-Razer@users.noreply.github.com>
2025-12-21 18:58:29 -0600

    Temp backdrop for building website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,56 +11,72 @@ import Image from "next/image";
 import Lenis from "lenis";
 
 const HomePage: React.FC = () => {
-  useEffect(() => {
-    const lenis = new Lenis();
+  // useEffect(() => {
+  //   const lenis = new Lenis();
 
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-  }, []);
+  //   function raf(time: number) {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
+  //   requestAnimationFrame(raf);
+  // }, []);
+  // return (
+  //   <>
+  //     <div className={styles.main}>
+  //       <TestHero />
+  //       <About />
+  //       <Film />
+  //       <Services />
+  //       <footer className={styles.footer}>
+  //         <div className={styles.nav}>
+  //           <p>
+  //             <a href="#">home</a>
+  //           </p>
+  //           <p>
+  //             <a href="">about</a>
+  //           </p>
+  //           <p>
+  //             <a href="">our work</a>
+  //           </p>
+  //           <p>
+  //             <a href="">contact</a>
+  //           </p>
+  //         </div>
+  //         <div className={styles.media}>
+  //           <a
+  //             href="https://www.instagram.com/recklessstudiosfilm/"
+  //             target="_blank"
+  //             rel="noopener noreferrer"
+  //           >
+  //             <Image
+  //               src="/instagram_svgrepo.com.svg"
+  //               width={24}
+  //               height={24}
+  //               alt="Instagram Icon"
+  //               className={styles.socialIcon}
+  //             />
+  //           </a>
+  //           {/* <p>{"[x]"}</p> */}
+  //         </div>
+  //       </footer>
+  //     </div>
+  //   </>
+  // );
   return (
-    <>
-      <div className={styles.main}>
-        <TestHero />
-        <About />
-        <Film />
-        <Services />
-        <footer className={styles.footer}>
-          <div className={styles.nav}>
-            <p>
-              <a href="#">home</a>
-            </p>
-            <p>
-              <a href="">about</a>
-            </p>
-            <p>
-              <a href="">our work</a>
-            </p>
-            <p>
-              <a href="">contact</a>
-            </p>
-          </div>
-          <div className={styles.media}>
-            <a
-              href="https://www.instagram.com/recklessstudiosfilm/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                src="/instagram_svgrepo.com.svg"
-                width={24}
-                height={24}
-                alt="Instagram Icon"
-                className={styles.socialIcon}
-              />
-            </a>
-            {/* <p>{"[x]"}</p> */}
-          </div>
-        </footer>
-      </div>
-    </>
+    <div className={styles.mainTemp}>
+      <h1 className={styles.header}>RECKLESS STUDIOS</h1>
+      <br />
+      <Image
+        src="/bulletPatienceSvg.svg"
+        width={100}
+        height={100}
+        alt="happy :D"
+        className={styles.image}
+      />
+      <br />
+      <p className={styles.text}>WE WILL BE WITH YOU SOON</p>
+      <p className={styles.text}>THANKS FOR VISITING.</p>
+    </div>
   );
 };
 
